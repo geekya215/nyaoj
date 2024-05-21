c143db503460e91243fc3c0824da4231cd28f589
Lin Yang <geekya215@gmail.com>
2024-05-21 17:28:37 +0800

    init maven project template and add deps

---
+++ src/main/java/io/geekya215/nyaoj/NyaojApplication.java
@@ -0,0 +1,13 @@
+package io.geekya215.nyaoj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NyaojApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NyaojApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/geekya215/nyaoj/NyaojApplicationTests.java
@@ -0,0 +1,13 @@
+package io.geekya215.nyaoj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NyaojApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

f0a10f25663c50fbdb36abc902f07d8d40693157
Lin Yang <geekya215@gmail.com>
2024-05-24 14:42:10 +0800

    add mybatis plus pagination interceptor

---
+++ src/main/java/io/geekya215/nyaoj/common/MybatisPlusConfiguration.java
@@ -0,0 +1,18 @@
+package io.geekya215.nyaoj.common;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MybatisPlusConfiguration {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        final MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
